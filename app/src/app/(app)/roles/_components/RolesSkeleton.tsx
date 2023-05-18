524e669246b48e9f6e7cf357316cdb0b5e138d69
Simon Knittel <hallo@simonknittel.de>
2023-05-18 12:41:45 +0200

    feat: Improve UX of loading skeleton for roles

--- app/src/app/(app)/roles/_components/RolesSkeleton.tsx
+++ app/src/app/(app)/roles/_components/RolesSkeleton.tsx
@@ -1,6 +1,6 @@
 const RolesSkeleton = () => {
   return (
-    <div className="mt-4 rounded bg-neutral-900 animate-pulse min-h-[20rem]" />
+    <div className="mt-4 rounded bg-neutral-900 animate-pulse min-h-[20rem] max-w-4xl" />
   );
 };
 
