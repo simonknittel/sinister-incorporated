f40649cfdb21c0c41730b3104fed1d8f3529ab01
Simon Knittel <hallo@simonknittel.de>
2023-05-18 12:38:50 +0200

    fix: Loading skeleton for roles

--- app/src/app/(app)/roles/_components/RolesSkeleton.tsx
+++ app/src/app/(app)/roles/_components/RolesSkeleton.tsx
@@ -1,5 +1,7 @@
 const RolesSkeleton = () => {
-  return <div className="mt-4 rounded bg-neutral-900 animate-pulse" />;
+  return (
+    <div className="mt-4 rounded bg-neutral-900 animate-pulse min-h-[20rem]" />
+  );
 };
 
 export default RolesSkeleton;
