bca17404117ed1eb0227cb4d256620650e6e6844
Simon Knittel <hallo@simonknittel.de>
2023-05-19 15:17:13 +0200

    feat: Add hint if no handle is indexed

--- app/src/app/(app)/spynet/search/_components/Citizen.tsx
+++ app/src/app/(app)/spynet/search/_components/Citizen.tsx
@@ -8,15 +8,19 @@ interface Props {
 const Citizen = ({ hit }: Props) => {
   return (
     <Link href={`/spynet/entity/${hit.objectID}`}>
-      <span className="flex gap-2 items-baseline">
-        <p>{hit.handles[0]}</p>
+      {hit.handles.length > 0 ? (
+        <span className="flex gap-2 items-baseline">
+          <p>{hit.handles[0]}</p>
 
-        {hit.handles.length > 1 && (
-          <p className="text-neutral-500 text-sm">
-            {hit.handles.slice(1).join(", ")}
-          </p>
-        )}
-      </span>
+          {hit.handles.length > 1 && (
+            <p className="text-neutral-500 text-sm">
+              {hit.handles.slice(1).join(", ")}
+            </p>
+          )}
+        </span>
+      ) : (
+        <p className="italic text-neutral-500">Unbekannt</p>
+      )}
 
       <span className="block text-sm text-neutral-500 mt-2">
         <p>Sinister ID: {hit.objectID}</p>
