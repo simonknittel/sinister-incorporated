22cba3e0db80ae28eeeaa543bc86c574a7652f25
Simon Knittel <hallo@simonknittel.de>
2023-05-28 09:50:27 +0200

    fix: Revert changes to TabPanel

--- app/src/app/_components/tabs/TabPanel.tsx
+++ app/src/app/_components/tabs/TabPanel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import clsx from "clsx";
 import { type ReactNode } from "react";
 import { useTabsContext } from "./TabsContext";
 
@@ -11,9 +12,10 @@ interface Props {
 const TabPanel = ({ children, id }: Props) => {
   const { activeTab } = useTabsContext();
 
-  if (activeTab !== id) return null;
+  // if (activeTab !== id) return null; // TODO: This will make forms with tabs (permissions) unusable since some of the inputs won't get initialized.
+  // return children;
 
-  return children;
+  return <div className={clsx({ hidden: activeTab !== id })}>{children}</div>;
 };
 
 export default TabPanel;
