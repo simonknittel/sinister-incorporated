a0621506ee4c4d8aa4326a1b2bde0a97c45a40c8
Simon Knittel <hallo@simonknittel.de>
2023-05-31 08:36:05 +0200

    feat: Implement useClickAway in Actions

--- app/src/app/_components/Actions.tsx
+++ app/src/app/_components/Actions.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { useClickAway } from "@uidotdev/usehooks";
 import clsx from "clsx";
-import { useState, type ReactNode } from "react";
+import {
+  useState,
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+} from "react";
 import { FaEllipsisH, FaTimes } from "react-icons/fa";
 import Button from "~/app/_components/Button";
 import styles from "./Actions.module.css";
@@ -23,18 +29,30 @@ const Actions = ({ children }: Props) => {
         {isOpen ? <FaTimes /> : <FaEllipsisH />}
       </Button>
 
-      {isOpen && (
-        <div
-          className={clsx(
-            "absolute top-[calc(100%+.5rem)] right-0 flex flex-col items-start gap-2 px-4 py-2 rounded bg-neutral-800 border border-neutral-900 z-10",
-            styles.actions
-          )}
-        >
-          {children}
-        </div>
-      )}
+      {isOpen && <Inner setIsOpen={setIsOpen}>{children}</Inner>}
     </div>
   );
 };
 
 export default Actions;
+
+interface InnerProps {
+  children?: ReactNode;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const Inner = ({ children, setIsOpen }: InnerProps) => {
+  const ref = useClickAway(() => setIsOpen(false));
+
+  return (
+    <div
+      className={clsx(
+        "absolute top-[calc(100%+.5rem)] right-0 flex flex-col items-start gap-2 px-4 py-2 rounded bg-neutral-800 border border-neutral-900 z-10",
+        styles.actions
+      )}
+      ref={ref}
+    >
+      {children}
+    </div>
+  );
+};
