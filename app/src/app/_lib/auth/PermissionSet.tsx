441d40a0028f470b633eca78f2c22cd9f335ef08
Simon Knittel <hallo@simonknittel.de>
2023-10-31 12:57:10 +0100

    refactor: Replace string with actual allowed values

--- app/src/app/_lib/auth/PermissionSet.tsx
+++ app/src/app/_lib/auth/PermissionSet.tsx
@@ -1,10 +1,45 @@
 export type PermissionSet = {
-  resource: string;
-  operation: string;
+  // TODO: Use ENUM (https://www.prisma.io/docs/concepts/components/prisma-schema/data-model#defining-enums)
+  resource:
+    | "login"
+    | "event"
+    | "operation"
+    | "orgFleet"
+    | "ship"
+    | "manufacturersSeriesAndVariants"
+    | "citizen"
+    | "noteType"
+    | "user"
+    | "role"
+    | "classificationLevel"
+    | "analytics"
+    | "lastSeen"
+    | "otherRole"
+    | "handle"
+    | "discordId"
+    | "note"
+    | "teamspeakId";
+  // TODO: Use ENUM (https://www.prisma.io/docs/concepts/components/prisma-schema/data-model#defining-enums)
+  operation:
+    | "create"
+    | "read"
+    | "readRedacted"
+    | "update"
+    | "delete"
+    | "manage"
+    | "confirm"
+    | "assign"
+    | "dismiss";
   attributes?: PermissionSetAttribute[];
 };
 
 export type PermissionSetAttribute = {
-  key: string;
+  // TODO: Use ENUM (https://www.prisma.io/docs/concepts/components/prisma-schema/data-model#defining-enums)
+  key:
+    | "confirmed"
+    | "noteTypeId"
+    | "classificationLevelId"
+    | "alsoUnconfirmed"
+    | "roleId";
   value: string | boolean;
 };
--- app/src/types.ts
+++ app/src/types.ts
@@ -1,5 +1,22 @@
 export type UserRole = null | "confirmed" | "admin";
+
 export type EntityLogConfirmationState =
   | "confirmed"
   | "falseReport"
   | undefined;
+
+// TODO: Use ENUM (https://www.prisma.io/docs/concepts/components/prisma-schema/data-model#defining-enums)
+export type EntityLogType =
+  | "handle"
+  | "note"
+  | "spectrum-id"
+  | "discordId"
+  | "teamspeakId"
+  | "role-added"
+  | "role-removed";
+
+// TODO: Use ENUM (https://www.prisma.io/docs/concepts/components/prisma-schema/data-model#defining-enums)
+export type EntityLogAttributeKey =
+  | "confirmed"
+  | "classificationLevelId"
+  | "noteTypeId";
