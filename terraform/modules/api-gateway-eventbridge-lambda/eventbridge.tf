4307afad71eb77a0abad0e49ac751bfe00add41c
Simon Knittel <hallo@simonknittel.de>
2024-02-07 15:28:18 +0100

    feat: Create event bridge rule and target through Terraform

--- terraform/modules/api-gateway-eventbridge-lambda/eventbridge.tf
+++ terraform/modules/api-gateway-eventbridge-lambda/eventbridge.tf
@@ -1,28 +1,22 @@
-# resource "aws_cloudwatch_event_rule" "main" {
-#   event_bus_name = var.event_bus.arn
+resource "aws_cloudwatch_event_rule" "main" {
+  event_bus_name = var.event_bus.arn
 
-#   event_pattern = jsonencode({
-#     detail-type = [
-#       var.event_bus_detail_type
-#     ]
-#   })
+  event_pattern = jsonencode({
+    detail-type = [
+      var.event_bus_detail_type
+    ]
+  })
+}
 
-#   # TODO
-#   # role_arn = ""
-# }
+resource "aws_cloudwatch_event_target" "main" {
+  rule           = aws_cloudwatch_event_rule.main.name
+  arn            = aws_lambda_function.main.arn
+  event_bus_name = var.event_bus.arn
+}
 
-# resource "aws_cloudwatch_event_target" "main" {
-#   rule           = aws_cloudwatch_event_rule.main.name
-#   arn            = aws_lambda_function.main.arn
-#   event_bus_name = var.event_bus.arn
-
-#   run_command_targets {
-#     key    = "tag:Name"
-#     values = ["FooBar"]
-#   }
-
-#   run_command_targets {
-#     key    = "InstanceIds"
-#     values = ["i-162058cd308bffec2"]
-#   }
-# }
+resource "aws_lambda_permission" "api_gateway" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.main.arn
+}
