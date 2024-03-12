499e9bb491fca10ea1f37b0045b94534fdc43ea0
Simon Knittel <hallo@simonknittel.de>
2024-03-12 18:56:06 +0100

    feat: Styling

--- app/src/app/(app)/dashboard/page.tsx
+++ app/src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { type Metadata } from "next";
 import { authenticatePage } from "~/_lib/auth/authenticateAndAuthorize";
+import { Hero } from "~/app/_components/Hero";
 import { CalendarTile } from "./_components/CalendarTile";
 import { ProfileTile } from "./_components/ProfileTile";
 import { QuotesTile } from "./_components/QuotesTile";
@@ -22,9 +23,11 @@ export default async function Page() {
 
   return (
     <main className="p-2 lg:p-8 pt-20">
-      <h1 className="sr-only">Dashboard</h1>
+      <div className="flex justify-center">
+        <Hero text="Sinister Inc" />
+      </div>
 
-      <div className={clsx(styles.tileGrid)}>
+      <div className={clsx(styles.tileGrid, "mt-8")}>
         {showEventsTile && <CalendarTile />}
         <ProfileTile />
         {/* <ShipsTile /> */}
