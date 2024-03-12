ac16a863f1a46ca16a0d51de9cfa43a16459cd92
Simon Knittel <hallo@simonknittel.de>
2024-03-12 19:50:14 +0100

    feat: Make sure to log Unleash errors

--- app/src/app/_lib/getUnleashFlag.ts
+++ app/src/app/_lib/getUnleashFlag.ts
@@ -1,5 +1,7 @@
 import { flag } from "@unleash/nextjs";
 import { cache } from "react";
+import { serializeError } from "serialize-error";
+import { log } from "~/_lib/logging";
 import { authenticate } from "../../_lib/auth/authenticateAndAuthorize";
 
 export const getUnleashFlag = cache(
@@ -14,15 +16,15 @@ export const getUnleashFlag = cache(
   ) => {
     const authentication = await authenticate();
 
-    const result = await flag(
-      name,
-      {
-        userId: authentication ? authentication.session.user.id : undefined,
-      },
-      {
-        fetchOptions: { next: { revalidate: 60 } },
-      },
-    );
+    const result = await flag(name, {
+      userId: authentication ? authentication.session.user.id : undefined,
+    });
+
+    if (result.error) {
+      log.error("Error fetching feature flag", {
+        error: serializeError(result.error),
+      });
+    }
 
     return result.enabled;
   },
