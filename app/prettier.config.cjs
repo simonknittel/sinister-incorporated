a47f585eb4afd5fa8f010c92491a29d03b1ea335
Simon Knittel <hallo@simonknittel.de>
2024-03-23 16:49:18 +0100

    chore: Update Prettier config

--- app/prettier.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss"), require.resolve("prettier-plugin-organize-imports")],
-};
-
-module.exports = config;
