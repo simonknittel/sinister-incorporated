09a24dc46250c9c557509f156563f1df7e509355
Simon Knittel <hallo@simonknittel.de>
2024-04-05 12:57:38 +0200

    chore: Update Vitest extension integration

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      provider: "istanbul",
       reporter: [
         "text", // For the terminal
-        "lcov", // For the VSCode extension and browser
       ],
     },
   },
