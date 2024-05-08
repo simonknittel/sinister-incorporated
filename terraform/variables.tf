28d946dc869e4c148a7da2258e0632bc1fb50fe9
Simon Knittel <hallo@simonknittel.de>
2024-05-08 18:51:26 +0200

    fix: remove deleted variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,11 +7,6 @@ variable "api_subdomain" {
   type = string
 }
 
-variable "email_function_mailgun_api_key" {
-  type      = string
-  sensitive = true
-}
-
 variable "email_function_parameters" {
   type = list(object({
     name  = string
