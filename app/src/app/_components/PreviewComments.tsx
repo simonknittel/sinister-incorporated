45ce9f6cf398768639039e228f25944a5b994258
Simon Knittel <hallo@simonknittel.de>
2024-05-08 13:01:02 +0200

    refactor: use named export instead of default export for PreviewComments

--- app/src/app/_components/PreviewComments.tsx
+++ app/src/app/_components/PreviewComments.tsx
@@ -1,10 +1,8 @@
 import { VercelToolbar } from "@vercel/toolbar/next";
 import { getUnleashFlag } from "../../lib/getUnleashFlag";
 
-const PreviewComments = async () => {
+export const PreviewComments = async () => {
   if (!(await getUnleashFlag("EnablePreviewComments"))) return null;
 
   return <VercelToolbar />;
 };
-
-export default PreviewComments;
