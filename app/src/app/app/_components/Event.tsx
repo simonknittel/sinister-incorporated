4f9a7c409589af97450d634692112b20e8b403d4
Simon Knittel <hallo@simonknittel.de>
2024-05-23 19:54:57 +0200

    feat: styling adjustments

--- app/src/app/app/_components/Event.tsx
+++ app/src/app/app/_components/Event.tsx
@@ -54,7 +54,7 @@ export const Event = async ({ className, event, index }: Props) => {
           </div>
         )}
 
-        <div className="flex-1 flex flex-col gap-2 p-8 lg:p-8 3xl:overflow-hidden">
+        <div className="flex-1 flex flex-col gap-2 p-4 lg:p-8 3xl:overflow-hidden">
           <h2 className="font-bold text-xl 3xl:text-ellipsis 3xl:whitespace-nowrap 3xl:overflow-hidden">
             {event.name}
           </h2>
@@ -80,13 +80,16 @@ export const Event = async ({ className, event, index }: Props) => {
           </p>
         </div>
 
-        <div className="flex-initial flex flex-col gap-2 p-8 pt-0 lg:pl-8 lg:pr-8 lg:pb-8 3xl:pt-8">
-          <DiscordButton path={`events/${event.guild_id}/${event.id}`} />
+        <div className="flex-initial flex 3xl:flex-col gap-2 p-4 pt-0 lg:pl-8 lg:pr-8 lg:pb-8 3xl:pt-8">
+          <DiscordButton
+            path={`events/${event.guild_id}/${event.id}`}
+            className="flex-1"
+          />
 
           {authentication.authorize("eventFleet", "read") && (
             <Link
               href={`/app/events/${event.id}`}
-              className="flex items-center justify-center gap-4 rounded uppercase h-11 border text-base border-sinister-red-500 text-sinister-red-500 hover:border-sinister-red-300 active:border-sinister-red-300 hover:text-sinister-red-300 active:text-sinister-red-300 px-6 whitespace-nowrap"
+              className="flex-1 flex items-center justify-center gap-4 rounded uppercase h-11 border text-base border-sinister-red-500 text-sinister-red-500 hover:border-sinister-red-300 active:border-sinister-red-300 hover:text-sinister-red-300 active:text-sinister-red-300 px-6 whitespace-nowrap"
             >
               Details
             </Link>
