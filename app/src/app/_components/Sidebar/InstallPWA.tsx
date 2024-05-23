543d534ebc29f013886cd577b5ce1230eee0d206
Simon Knittel <hallo@simonknittel.de>
2024-05-23 19:43:06 +0200

    feat: add <InstallPWA>

---
+++ app/src/app/_components/Sidebar/InstallPWA.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import clsx from "clsx";
+import { useEffect, useRef } from "react";
+import {
+  MdOutlineInstallDesktop,
+  MdOutlineInstallMobile,
+} from "react-icons/md";
+
+type Props = Readonly<{
+  className?: string;
+}>;
+
+export const InstallPWA = ({ className }: Props) => {
+  const installPrompt = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      installPrompt.current = e;
+    });
+  }, []);
+
+  const handleClick = async () => {
+    if (!installPrompt.current) return;
+    await installPrompt.current.prompt();
+  };
+
+  return (
+    <button
+      className={clsx(
+        className,
+        "text-neutral-600 text-center text-sm hover:text-neutral-400 active:text-neutral-300 inline-flex gap-2 items-center group",
+      )}
+      type="button"
+      onClick={handleClick}
+    >
+      App installieren
+      <span className="p-1 rounded border border-neutral-800 group-hover:border-neutral-600 group-active:border-neutral-500">
+        <MdOutlineInstallMobile className="lg:hidden" />
+        <MdOutlineInstallDesktop className="hidden lg:block" />
+      </span>
+    </button>
+  );
+};
