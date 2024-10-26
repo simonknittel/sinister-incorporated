4d0d84104e36b6f2cd668f853d5d9f52c391f414
Simon Knittel <hallo@simonknittel.de>
2024-10-26 20:37:39 +0200

    feat(Terraform): use version ranges

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
 terraform {
-  required_version = "1.9.6"
+  required_version = "~> 1.9"
 
   backend "s3" {}
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.67.0"
+      version = "~> 5.67"
     }
 
     external = {
       source  = "hashicorp/external"
-      version = "2.3.4"
+      version = "~> 2.3.4"
     }
 
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.42.0"
+      version = "~> 4.42.0"
     }
   }
 }
