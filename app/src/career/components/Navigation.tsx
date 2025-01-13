a1bfbfceaca11c9db03cda2d06ed629f4d2e6360
Simon Knittel <hallo@simonknittel.de>
2025-01-13 10:55:41 +0100

    feat(Career): add position for navigation

--- app/src/career/components/Navigation.tsx
+++ app/src/career/components/Navigation.tsx
@@ -15,23 +15,25 @@ export const Navigation = ({ className, flows }: Props) => {
 
   return (
     <div className={clsx("flex justify-center", className)}>
-      {flows.map((flow) => (
-        <Link
-          key={flow.id}
-          href={`/app/career/${flow.id}`}
-          className={clsx(
-            "first:rounded-l last:rounded-r border border-sinister-red-500 h-8 flex items-center justify-center px-3 gap-2 uppercase",
-            {
-              "bg-sinister-red-500 text-white":
-                pathname === `/app/career/${flow.id}`,
-              "text-sinister-red-500 hover:text-sinister-red-300 hover:border-sinister-red-300":
-                pathname !== `/app/career/${flow.id}`,
-            },
-          )}
-        >
-          {flow.name}
-        </Link>
-      ))}
+      {flows
+        .toSorted((a, b) => (a.position ?? 0) - (b.position ?? 0))
+        .map((flow) => (
+          <Link
+            key={flow.id}
+            href={`/app/career/${flow.id}`}
+            className={clsx(
+              "first:rounded-l last:rounded-r border border-sinister-red-500 h-8 flex items-center justify-center px-3 gap-2 uppercase",
+              {
+                "bg-sinister-red-500 text-white":
+                  pathname === `/app/career/${flow.id}`,
+                "text-sinister-red-500 hover:text-sinister-red-300 hover:border-sinister-red-300":
+                  pathname !== `/app/career/${flow.id}`,
+              },
+            )}
+          >
+            {flow.name}
+          </Link>
+        ))}
     </div>
   );
 };
