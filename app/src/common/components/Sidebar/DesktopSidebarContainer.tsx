9bd1f604456d134f04ed35737957120997145779
Simon Knittel <hallo@simonknittel.de>
2025-03-05 17:24:03 +0100

    feat(common): styling

--- app/src/common/components/Sidebar/DesktopSidebarContainer.tsx
+++ app/src/common/components/Sidebar/DesktopSidebarContainer.tsx
@@ -4,10 +4,10 @@ import { DesktopSidebar } from "./DesktopSidebar";
 
 export const DesktopSidebarContainer = () => {
   return (
-    <div className="hidden lg:flex fixed left-8 top-8 bottom-8 w-96 flex-col">
+    <div className="hidden lg:flex fixed left-0 top-0 bottom-0 w-[26rem] flex-col">
       <Suspense
         fallback={
-          <div className="flex max-h-full h-96 flex-col justify-between bg-neutral-800/50 rounded-2xl">
+          <div className="flex max-h-full h-96 ml-8 my-8 flex-col justify-between bg-neutral-800/50 rounded-2xl">
             <div />
 
             <Footer className="px-8 pb-4 pt-0" />
