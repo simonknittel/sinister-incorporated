ddcc1677022dcef8e8ee7ae0c7bcf548ee6aee8c
Simon Knittel <hallo@simonknittel.de>
2025-03-22 11:55:34 +0100

    feat(Events): add SQL migration for dropping old column (closes #1187)

---
+++ app/prisma/migrations/20250322105453_drop_old_required_variant_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `requiredVariantId` on the `EventPosition` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EventPosition" DROP COLUMN "requiredVariantId";
