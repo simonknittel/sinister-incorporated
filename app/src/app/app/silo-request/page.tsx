16ed17699bbf6cc750aa34ffbd7d0dad25333df6
Simon Knittel <hallo@simonknittel.de>
2025-08-31 12:38:51 +0200

    refactor: simplify iframe pages

--- app/src/app/app/silo-request/page.tsx
+++ app/src/app/app/silo-request/page.tsx
@@ -1,8 +1,6 @@
 import { authenticatePage } from "@/auth/server";
-import { Button2 } from "@/common/components/Button2";
-import { Link } from "@/common/components/Link";
+import { IframePage } from "@/common/components/IframePage";
 import { type Metadata } from "next";
-import { FaExternalLinkAlt, FaSpinner } from "react-icons/fa";
 
 export const revalidate = 86400; // 24 hours
 
@@ -14,27 +12,6 @@ export default async function Page() {
   await authenticatePage("/app/silo-request");
 
   return (
-    <main className="pb-16 lg:px-2 lg:pb-2 relative">
-      <iframe
-        // src="https://forms.gle/mNnrAA6mHF3sh3mm9"
-        src="https://docs.google.com/forms/d/e/1FAIpQLSeHEgpv4GmnZhu7MS2aQc9zgETWQw8tusJ7oaGLsyuHeD1LMw/viewform"
-        className="w-full h-[calc(100dvh-4rem)] lg:h-[calc(100dvh-4.75rem)] relative z-10"
-        title="Formular für eine SILO-Anfrage"
-      />
-
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <FaSpinner className="animate-spin text-5xl text-neutral-500" />
-      </div>
-
-      <Button2
-        as={Link}
-        href="https://forms.gle/mNnrAA6mHF3sh3mm9"
-        title="In einem neuen Tab öffnen"
-        variant="secondary"
-        className="absolute right-3 top-1 z-10"
-      >
-        <FaExternalLinkAlt />
-      </Button2>
-    </main>
+    <IframePage url="https://docs.google.com/forms/d/e/1FAIpQLSeHEgpv4GmnZhu7MS2aQc9zgETWQw8tusJ7oaGLsyuHeD1LMw/viewform" />
   );
 }
