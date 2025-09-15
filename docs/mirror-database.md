b5908aa8a11c6744737e55038bd67a86f209a876
Simon Knittel <hallo@simonknittel.de>
2025-09-15 21:47:10 +0200

    chore: add script for mirroring production database to stage

--- docs/mirror-database.md
+++ docs/mirror-database.md
@@ -2,4 +2,9 @@
 
 > How to copy the production database to your local machine
 
-1. `./scripts/mirror-database.sh "postgresql://<user>:<pass>@<host>:5432/<database>"`
+1. `./scripts/mirror-database-production-to-local.sh "postgresql://<user>:<pass>@<host>:5432/<database>"`
+
+> How to copy the production database to the stage database
+
+1. `./scripts/mirror-database-production-to-stage.sh "postgresql://<user>:<pass>@<host>:5432/production" "postgresql://<user>:<pass>@<host>:5432/stage"`
+2. `cd app && nvm install && DATABASE_URL="postgresql://<user>:<pass>@<host>:5432/stage" npx prisma db push`
--- scripts/mirror-database-production-to-local.sh
+++ scripts/mirror-database-production-to-local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Usage: `./scripts/mirror-database.sh postgresql://<user>:<pass>@<host>:5432/<database>`
+# Usage: `./scripts/mirror-database-production-to-local.sh "postgresql://<user>:<pass>@<host>:5432/<database>"`
 # # See `docs/mirror-database.md` for more information
 
 # Exit immediately if a command exits with a non-zero status.
@@ -11,9 +11,9 @@ CONNECTION_STRING=$1
 docker compose rm --stop --force psql
 docker compose up --detach psql
 
-docker exec --interactive sinister-incorporated-psql-1 /bin/bash <<EOF
+docker container exec --interactive sinister-incorporated-psql-1 /bin/bash <<EOF
 	pg_dump --dbname=$CONNECTION_STRING --no-owner --no-privileges --no-acl --no-tablespaces --format=custom --file dump && \
 		pg_restore --dbname=postgresql://postgres:admin@localhost:5432/db --no-owner --clean --if-exists dump
 EOF
 
-echo "✅ Successfully mirrored database"
+echo "✅ Successfully mirrored database (production to local)"
