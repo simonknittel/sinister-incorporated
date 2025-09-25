4a1cb5927527c23c20b6da331653443703a866d0
Simon Knittel <hallo@simonknittel.de>
2025-09-25 11:43:58 +0200

    feat(Cloudwatch): separate alarms

--- terraform/modules/scheduled-lambda/cloudwatch.tf
+++ terraform/modules/scheduled-lambda/cloudwatch.tf
@@ -35,25 +35,3 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   period              = 60
   alarm_description   = "This alarm detects a high number of errors for this Lambda function."
 }
-
-resource "aws_cloudwatch_metric_alarm" "deadletter_message_count" {
-  alarm_name = "deadletter-message-count-${var.function_name}"
-
-  namespace   = "AWS/SQS"
-  metric_name = "ApproximateNumberOfMessagesVisible"
-
-  # Related: https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-metrics-and-dimensions.html
-  dimensions = {
-    QueueName = aws_sqs_queue.main_deadletter.name
-  }
-
-  statistic           = "Sum"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold           = 1
-  evaluation_periods  = 1
-  datapoints_to_alarm = 1
-  period              = 60
-  alarm_description   = "This alarm detects if there are messages in the dead letter queue."
-
-  alarm_actions = []
-}
