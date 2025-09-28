0da18ce087b63e1471802a18cd14bdb97ff8961f
Simon Knittel <hallo@simonknittel.de>
2025-09-28 14:06:34 +0200

    feat(Terraform): add query for Max Memory Used

--- terraform/modules/scheduled-lambda/cloudwatch.tf
+++ terraform/modules/scheduled-lambda/cloudwatch.tf
@@ -35,3 +35,19 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   period              = 60
   alarm_description   = "This alarm detects a high number of errors for this Lambda function."
 }
+
+resource "aws_cloudwatch_query_definition" "max_memory_used" {
+  name = "Max Memory Used (${var.function_name})"
+
+  log_group_names = [
+    "/aws/lambda/${var.function_name}",
+  ]
+
+  query_string = <<EOF
+fields @timestamp, @message
+| sort @timestamp desc
+| limit 100
+| filter @message like "Max Memory Used"
+| parse @message "Max Memory Used: * MB" as maxMemoryUsedMB
+EOF
+}
