79613166c65f408612998dead10c33f79606065f
Simon Knittel <hallo@simonknittel.de>
2025-10-01 18:45:34 +0200

    refactor(Apps): use client-side rendering for new filter (closes #1638)

--- app/src/modules/apps/components/Filters.tsx
+++ app/src/modules/apps/components/Filters.tsx
@@ -3,17 +3,23 @@
 import { Button2 } from "@/modules/common/components/Button2";
 import { Link } from "@/modules/common/components/Link";
 import clsx from "clsx";
+import { useCallback, type MouseEvent } from "react";
 import type { AppList } from "../utils/types";
 
 interface Props {
   readonly className?: string;
   readonly appLinks?: AppList | null;
   readonly selectedTags?: string[];
+  readonly setSelectedTags?: (tags: string[]) => void;
 }
 
-export const Filters = ({ className, appLinks, selectedTags }: Props) => {
-  const filters = new Map<string, string>();
-  filters.set("all", "Alle");
+export const Filters = ({
+  className,
+  appLinks,
+  selectedTags,
+  setSelectedTags,
+}: Props) => {
+  const filters = new Map<string, string>([["all", "Alle"]]);
   for (const appLink of appLinks || []) {
     if ("tags" in appLink && appLink.tags?.length) {
       for (const tag of appLink.tags) {
@@ -21,6 +27,15 @@ export const Filters = ({ className, appLinks, selectedTags }: Props) => {
       }
     }
   }
+
+  const handleClick = useCallback(
+    (event: MouseEvent, tag: string) => {
+      if (!setSelectedTags) return;
+      event.preventDefault();
+      setSelectedTags([tag]);
+    },
+    [setSelectedTags],
+  );
 
   return (
     <div className={clsx("flex flex-wrap gap-2 justify-center", className)}>
@@ -30,6 +45,8 @@ export const Filters = ({ className, appLinks, selectedTags }: Props) => {
           href={`/app/apps?tag=${key}`}
           variant={selectedTags?.includes(key) ? "primary" : "secondary"}
           key={key}
+          replace
+          onClick={(e) => handleClick(e, key)}
         >
           {label.charAt(0).toUpperCase() + label.slice(1)}
         </Button2>
